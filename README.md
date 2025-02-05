5c069a2b252f0ce898c6a702b5495fcc1b05951f
Poluianov Valeriy <bestcom5055@gmail.com>
2025-02-05 08:20:48 +0300

    Kittybot/Feat: TG-bot API practice

---
+++ README.md
@@ -0,0 +1,8 @@
+# Yandex. Backend python developer course. 2024-2025
+
+- `/tic-tac-toe/` - Tic-tac-toe game in terminal;
+- `/the-snake/` - Snake game via pygame;
+- `/anfisa/` - Blog site about ice cream;
+- `/blogicum/` - Site for publishing articles;
+- `/kittybot/` - Telegram-bot sending photos of cats;
+- `/api_yatube/` - 
---
+++ kittybot/main.py
@@ -0,0 +1,45 @@
+import requests
+from telebot import TeleBot, types
+
+bot = TeleBot(token='7632787987:AAFpqkjSWx0Gv2wA5uGC6-HwTZt1QMvHh2I')
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message):
+    chat = message.chat
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    chat = message.chat
+    name = message.chat.first_name
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_newcat = types.KeyboardButton('/newcat')
+    keyboard.add(button_newcat)
+
+    bot.send_message(
+        chat_id=chat.id,
+        text=f'Привет, {name}. Посмотри, какого котика я тебе нашёл',
+        reply_markup=keyboard,
+    )
+
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler(content_types=['text'])
+def say_hi(message):
+    chat = message.chat
+    chat_id = chat.id
+    bot.send_message(chat_id=chat_id, text='Привет, я KittyBot!')
+
+
+if __name__ == '__main__':
+    bot.polling()
