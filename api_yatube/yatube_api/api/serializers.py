8be969f89fdefbd24306ef226a00d4a5416532d1
Poluianov Valeriy <bestcom5055@gmail.com>
2025-02-05 13:56:52 +0300

    YaTube/Feat: CRUD API

---
+++ api_yatube/yatube_api/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from posts.models import Post, Group, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'pub_date', 'image', 'group')
+        read_only_fields = ('id', )
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+        read_only_fields = ('id', )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post',  'text', 'created')
+        read_only_fields = ('id', )
