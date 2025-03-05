65e92850109926b146f683f0b1b34b9295e3df44
Gutsy <bestcom5055@gmail.com>
2025-03-05 05:51:15 +0300

    Kittygram/Server: Make infra via nginx n gunicorn.

--- kittygram/backend/kittygram_backend/settings.py
+++ kittygram/backend/kittygram_backend/settings.py
@@ -5,9 +5,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'localhost',
+    '172.18.13.195',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
